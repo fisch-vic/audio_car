3fd908df238678cb552af1ccbef04f714c3742dc
fisch-vic <vf82598@uga.edu>
2022-01-14 18:24:39 -0500

    major changes

--- audioCar.py
+++ audioCar.py
@@ -1,34 +1,185 @@
 import RPi.GPIO as GPIO
 import time
+import statistics as stat
+import random
+
+class audioCar:
+    def __init__(self):
+        # function to initialize car
+        # GPIO uses pin number
+        GPIO.setmode(GPIO.BCM)
+
+        # hbridge pin definitions:
+
+        self.hbridge = {"1A":18,"2A":27,"3A":22,"4A":23}
+        self.encoder = {"left":4, "right":17}
+        self.led = {"a":19, "b":20, "c":21}
+        self.td = 0.1
+        self.encoder_time = {"4":0, "17":0}
+        self.time_diff = {"4":0, "17":0}
+        self.rpm = {"4":0, "17":0}
+        self.speed = {"4":0, "17":0} 
+
+        self.rpm_log = {"4":[], "17":[]} 
+
+        self.k1 = 1
+        self.k2 = 1
+        self.k3 = 1
+
+        
+
+        # hbridge GPIO init
+        for pin in self.hbridge:
+            # set as ouptut
+            GPIO.setup(self.hbridge[pin], GPIO.OUT)
+            # set low
+            GPIO.output(self.hbridge[pin], GPIO.LOW)
+
+        self.h1A = GPIO.PWM(self.hbridge["1A"],60)
+        self.h2A = GPIO.PWM(self.hbridge["2A"],60)
+        self.h3A = GPIO.PWM(self.hbridge["3A"],60)
+        self.h4A = GPIO.PWM(self.hbridge["4A"],60)
+
+        self.h1A.start(0)
+        self.h2A.start(0)
+        self.h3A.start(0)
+        self.h4A.start(0)
+
+        self.pwm = {"4":50, "17":50} 
 
 
-def init():
-    # function to initialize car
-    # GPIO uses pin number
-    GPIO.setmode(GPIO.BOARD)
+        # encoder init
+        for pin in self.encoder:
+            # set as input
+            GPIO.setup(self.encoder[pin], GPIO.IN)
+            GPIO.add_event_detect(self.encoder[pin], GPIO.FALLING, callback=self.encoder_callback,bouncetime=50)
+    
 
-    # hbridge pin definitions:
-
-    hbridge = {"1A":7,"2A":11,"3A":13,"4A":15}
-
-    # hbridge GPIO init
-    for pin in hbridge:
-        # set as ouptut
-        GPIO.setup(hbridge[pin], GPIO.OUT)
-        # set low
-        GPIO.output(hbridge[pin], GPIO.LOW)
-
-def exit():
-    # function to exit
-    GPIO.cleanup()
+        # led init 
+        for pin in self.led:
+            GPIO.setup(self.led[pin], GPIO.OUT)
+            GPIO.output(self.led[pin], GPIO.HIGH)
+            time.sleep(self.td)
+            GPIO.output(self.led[pin], GPIO.LOW)
 
 
-for pin in hbridge:
-    print(pin)
-    GPIO.output(hbridge[pin], GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(hbridge[pin], GPIO.LOW)
-    time.sleep(1)
+    def exit(self):
+        # function to exit
+        self.h1A.stop()
+        self.h2A.stop()
+        self.h3A.stop()
+        self.h4A.stop()
+        GPIO.cleanup()
+
+
+    def hbridge_test(self):
+        # test sequence for hbridge
+        for pin in self.hbridge:
+            print(pin)
+            GPIO.output(self.hbridge[pin], GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(self.hbridge[pin], GPIO.LOW)
+            time.sleep(self.td)
+
+    def encoder_callback(self,enc):
+        self.time_diff[str(enc)]= time.time() - self.encoder_time[str(enc)]
+        self.encoder_time[str(enc)] = time.time()
+        self.rpm[str(enc)] = (60/(self.time_diff[str(enc)]*20))
+        self.rpm_log[str(enc)].append(self.rpm[str(enc)])
+        diff = (abs(self.speed[str(enc)])- self.rpm[str(enc)])
+        self.pwm[str(enc)] += (pow(diff,2)*self.k1 + diff*self.k2 + self.k3)
+
+        if self.pwm[str(enc)] > 100:
+            self.pwm[str(enc)] = 100
+        # don't stall out
+        if self.pwm[str(enc)] < 20:
+            self.pwm[str(enc)] = 20
+        # but still stop
+        if self.speed[str(enc)] == 0: 
+            self.pwm[str(enc)] = 0
+
+        if enc == 4 and self.speed[str(enc)] >= 0:
+            self.h1A.ChangeDutyCycle(int(self.pwm[str(enc)]))
+            self.h2A.ChangeDutyCycle(0)
+        elif enc == 4 and self.speed[str(enc)] <= 0:
+            self.h2A.ChangeDutyCycle(int(self.pwm[str(enc)]))
+            self.h1A.ChangeDutyCycle(0)
+        elif enc == 17 and self.speed[str(enc)] >= 0:
+            self.h3A.ChangeDutyCycle(int(self.pwm[str(enc)]))
+            self.h4A.ChangeDutyCycle(0)
+        elif enc == 17 and self.speed[str(enc)] <= 0:
+            self.h4A.ChangeDutyCycle(int(self.pwm[str(enc)]))
+            self.h3A.ChangeDutyCycle(0)
+ 
+ 
+        #print("encoder: " + str(enc) + " rpm: " + str(self.rpm[str(enc)]) + ",  " + str(self.pwm[str(enc)]))
+
+    def feedback_stats(self):
+
+        l = self.rpm_log["4"]
+        r = self.rpm_log["17"]
+
+        self.lmean = stat.mean(l)
+        self.rmean = stat.mean(r)
+
+        self.lstd = stat.stdev(l)
+        self.rstd = stat.stdev(r)
+
+        
+        print("Left(4): mean: " + str(self.lmean) + " standard deviation: " + str(self.lstd))
+        print("Right(17): mean: " + str(self.rmean) + " standard deviation: " + str(self.rstd))
+
+    def rando_opto(self):
+        for i in range(100):
+            self.rpm_log = {"4":[], "17":[]} 
+            self.k1 = random.randrange(-100,100) / 500 
+            self.k2 = random.randrange(-500,500) / 500
+            self.k3 = random.randrange(-500,500) / 500 
+            print("k1 = " + str(self.k1) + " k2 = " + str(self.k2) + " k3 = " + str(self.k3))
+            self.motor_test()
+            self.feedback_stats()
+
+
+            text = ("k1 = " + str(self.k1) + " k2 = " + str(self.k2) + " k3 = " + str(self.k3) + " Left(4)- mean: " + str(self.lmean) + " standard deviation: " + str(self.lstd) + " Right(17)- mean: " + str(self.rmean) + " standard deviation: " + str(self.rstd) + " quality = " + str(round(1/((self.speed["4"]-self.lmean)*self.lstd) * 1/((self.speed["17"]-self.rmean)*self.rstd),4)) + '\n')
+
+            f = open("rando_opto.txt", "a")
+            f.write(text)
+            f.close()
+
+
+    def motor_test(self):
+
+        for i in range(5):
+
+            self.pwm = {"4":50, "17":50} 
+
+            self.speed["4"] = 45 
+            self.speed["17"] = 45
+            self.h1A.ChangeDutyCycle(100)
+            self.h3A.ChangeDutyCycle(100)
+            time.sleep(3)
+            
+            self.pwm = {"4":50, "17":50} 
+
+            self.speed["4"] = -45 
+            self.speed["17"] = -45
+            self.h2A.ChangeDutyCycle(100)
+            self.h4A.ChangeDutyCycle(100)
+            time.sleep(3)
+
+            self.speed["4"] = 0 
+            self.speed["17"] = 0 
+
+        time.sleep(4)
+
+
+
+            # maintain spee4`
+
+
+ac = audioCar()
+ac.rando_opto()
+ac.exit()
 
 
 
